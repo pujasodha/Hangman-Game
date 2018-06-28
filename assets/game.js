fc63c889e99dc6d3843d19222229f77171a845d7
pujasodha <pujasodha@gmail.com>
2018-06-28 09:57:40 -0700

    Reset letters and word on new game

--- assets/game.js
+++ assets/game.js
@@ -1,4 +1,4 @@
-var wordBank = ['Annyong', 'Banana', 'Seal', "Her", 'Nude', 'Touching'];
+var wordBank = ['Annyong', 'Banana', 'Seal', 'Her', 'Nude', 'Touching'];
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ];
 
